590be5e8b7a43371509118d50cd1f1a810f066b0
Halatnikov <43994658+Halatnikov@users.noreply.github.com>
2021-06-24 18:08:58 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1624532662,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"statictiledbg.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"russian.json",
+		"valenok.json"
+	]
+}
